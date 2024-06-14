83ae0164fef108640b867ea405a01cebca6cc02e
kpj307 <kpj307@gmail.com>
2024-06-14 13:47:08 +0300

    edited routes

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import upload_view, extracted_data_view
 
 urlpatterns = [
-    path('upload/', upload_view, name='upload'),
+    path('', upload_view, name='upload'),
     path('extracted-data/', extracted_data_view, name='extracted-data'),
 ]
